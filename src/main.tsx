e4abaa3b5a0602d4848ada9f18b8e8139d47f46c
dhilip kumar <dk9361880@gmail.com>
2025-09-18 19:40:25 +0530

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
+<SpeedInsights/>
 createRoot(document.getElementById('root')!).render(<App />);
